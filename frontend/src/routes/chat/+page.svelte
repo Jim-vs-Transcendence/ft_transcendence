6c1da42057a66840b9fb623fd4ba2cd5c341fefe
adm <adm@bases-MacBook-Air.local>
2023-06-10 00:15:20 +0900

    done refresh on realtime

--- frontend/src/routes/chat/+page.svelte
+++ frontend/src/routes/chat/+page.svelte
@@ -1,12 +1,24 @@
 <script lang='ts'>
 	import { goto } from "$app/navigation";
 	import { io_chat } from "$lib/webSocketConnection_chat";
+	import { each } from "svelte/internal";
 
-	io_chat;
+	let	rooms_list : string [] = [];
+
+
+	io_chat.on("room-refresh", ( data : string []) => {
+		rooms_list = [...data ]
+	});
+
 
 	function ft_goto_chatroom(room_select: string)
 	{
 		goto('/chat/' + room_select);
-	}
+	};
 
 </script>
+<lu>
+{#each rooms_list as room}
+	<li id="room" on:keydown={ () => { ft_goto_chatroom(room) } }  > {room} </li>
+{/each}
+</lu>
