82ec8b00c4d7bcf30f4abbce1e59d42c4398681a
adm <adm@bases-MacBook-Air.local>
2023-06-10 04:51:24 +0900

    join update  - allways refresh when join chat channel

--- frontend/src/routes/chat/+page.svelte
+++ frontend/src/routes/chat/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+
 import { goto } from '$app/navigation';
 import { io_chat } from '$lib/webSocketConnection_chat';
 
@@ -10,20 +11,32 @@ io_chat.on('room-refresh', (data: ChatRoomIF []) => {
     rooms_list = [...data];
 });
 
-
 io_chat.on('room-create', (data: ChatRoomIF) => {
 	if (!data.room_name)
 		console.log("생성 불가");
     console.log(data);
 });
 
+io_chat.on('room-join', (data: ChatRoomIF) => {
+	if (!data.room_name)
+	{
+		console.log("접속 불가");
+		io_chat.emit("room-refresh", "room-join error");
+	}
+	goto("/chat/" + data.room_name);
+});
+
 function JoinRoom(room_select: ChatRoomIF) {
-    goto('/chat/' + room_select.room_name);
+	console.log();
+	io_chat.emit("room-join", room_select);
 };
 
 function CreateRoom(): boolean{
 	if (!room_name)
+	{
+		alert("방이름을 입력하세요");
 		return false;
+	}
 	// if (room_password) // 이건 옵션이다
 	// 	return false;
     io_chat.emit('room-create', {"room_name": room_name, "room_password": room_password} );
@@ -46,7 +59,7 @@ function ft_room_create_keydown(e : KeyboardEvent) {
 
 <lu>
     {#each rooms_list as room}
-    <li id="room" on:keydown={() => { JoinRoom(room) }}>
+    <li id="room" on:mousedown={() => { JoinRoom(room) }} style="padding: 20px; color: #00a; background-color: #aa3; width: 50%; margin: auto; border: solid #455 11px;" >
         {room.room_name}
     </li>
     {/each}
