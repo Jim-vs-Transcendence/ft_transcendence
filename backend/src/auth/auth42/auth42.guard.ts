4c4bd6d64d3c853a89a3cbb7d6ccb2352f9d07df
dhyun <ehdghho@kakao.com>
2023-06-14 08:18:22 +0000

    [REFACT]: 42Auth 인가 거부시 처리를 위한 authFilter 추가

--- backend/src/auth/auth42/auth42.guard.ts
+++ backend/src/auth/auth42/auth42.guard.ts
@@ -1,31 +1,12 @@
-import {
-  ExecutionContext,
-  Injectable,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class AuthGuard42 extends AuthGuard('42') {
-  canActivate(context: ExecutionContext) {
-    return super.canActivate(context);
-  }
-
-  handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
-    const res = context.switchToHttp().getResponse();
-
-    if (err) {
-      throw new InternalServerErrorException({
-        msg: '42 인증 에러',
-        err,
-        info,
-      });
+  handleRequest(err: any, user: any) {
+    if (err || !user) {
+      throw err || new UnauthorizedException();
     }
-
-    if (!user) {
-      res.redirect('http://localhost:5173');
-    }
-
     return user;
   }
 }
