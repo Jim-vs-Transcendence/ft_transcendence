ffa98cc68ab3d3b607da00d7901e36d9f83c64db
Victra15 <ioosulsa@gmail.com>
2023-06-20 00:49:08 +0900

    [FEAT]: chatroom option menu design change

--- frontend/src/components/Chat/ChatUserOptions.svelte
+++ frontend/src/components/Chat/ChatUserOptions.svelte
@@ -13,12 +13,12 @@
 
 </script>
 
-<div class="card p-4 z-10 column-count-1" data-popup={chatUser._user_info.id}>
-	<div><p class="cursor-point">profile</p></div>
-	<div><button class="cursor-pointer" on:click={()=>{fn("invite");}}>invite game {chatUser._user_info.id}</button></div>
-	<div><button class="cursor-pointer" on:click={()=>{fn("mute");}}>mute</button></div>
-	<div><button class="cursor-pointer" on:click={()=>{fn("kick");}}>kick</button></div>
-	<div><button class="cursor-pointer" on:click={()=>{fn("ban");}}>ban</button></div>
-	<div><button class="cursor-pointer" on:click={()=>{fn("appoint");}}>appoint</button></div>
+<div class="card p-2 z-10 column-count-1" data-popup={chatUser._user_info.id}>
+	<div class="py-2"><p class="cursor-point">profile</p></div>
+	<div class="hover:bg-violet-300"><button class="cursor-pointer font-sans md:font-serif" on:click={()=>{fn("invite");}}>놀이 초대 {chatUser._user_info.id}</button></div>
+	<div class="hover:bg-violet-300"><button class="cursor-pointer font-sans md:font-serif" on:click={()=>{fn("mute");}}>멈춰✋</button></div>
+	<div class="hover:bg-violet-300"><button class="cursor-pointer font-sans md:font-serif" on:click={()=>{fn("kick");}}>내보내기</button></div>
+	<div class="hover:bg-violet-300"><button class="cursor-pointer font-sans md:font-serif" on:click={()=>{fn("ban");}}>영구추방</button></div>
+	<div class="hover:bg-violet-300"><button class="cursor-pointer font-sans md:font-serif" on:click={()=>{fn("appoint");}}>부방장 임명</button></div>
 	<div class="arrow bg-surface-100-800-token" />
 </div>
