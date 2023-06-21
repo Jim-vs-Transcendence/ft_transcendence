dab071c0174f836cfcd87d6db07b59720d0625f4
hodongho <ehdghho@kakao.com>
2023-06-21 04:29:49 +0000

    [FEAT]: updateUserStauts 추가, token정보 확인 시 온라인 상태로 변경하는 로직 추가
    - user_status만 변경하는 함수 생성
    - /user/stauts/:id 엔드포인트 추가
    - isLogin시 status가 0(offline)이라면 1(online)로 변경해주는 로직 추가

--- backend/src/auth/token/token.controller.ts
+++ backend/src/auth/token/token.controller.ts
@@ -29,7 +29,14 @@ export class TokenController {
     if (!req.user) return false;
 
     // const test = req.cookies['authToken'];
-    return await this.usersService.findOne(req.user);
+    const user = await this.usersService.findOne(req.user);
+    if (user.user_status === 0) {
+      user.user_status = 1;
+      this.usersService.updateUser(req.user, user);
+    }
+    return user;
+    // this.usersService.updateUserStatus(req.user, 1);
+    // return await this.usersService.findOne(req.user);
   }
 
   @ApiOperation({
