0965d28a9fcf1921a672333fb395a87365cb7637
hodongho <ehdghho@kakao.com>
2023-07-03 05:02:47 +0000

    [FEAT]: 세션 스토리지를 사용한 중복로그인 방지로직 추가
    - 세션 스토리지 관리를 통한 유저 로그인 관리하면서 exitPage 대신 바로 메인으로 내보내도록 수정
    - client 변조에 대한 문제를 해결하려면 소켓 인터페이스에 토큰정보를 가지고 있어야할 것으로 보임

--- backend/src/auth/token/token.guard.ts
+++ backend/src/auth/token/token.guard.ts
@@ -9,8 +9,12 @@ export class TokenGuard implements CanActivate {
       const req = await context.switchToHttp().getRequest();
       //헤더
       const token = await req.header('authtoken');
+      const login = await req.header('islogin');
       // const token = await req.cookies['auth_token'];
       const userId = await this.tokenService.verifyToken(token);
+      if (!await this.tokenService.verifyLogin(userId.toString(), login)) {
+        return false;
+      }
 
       req.user = userId;
       return userId;
